cbc3b19902b4976d416550641d3d917b220e3023
nguyenanhtuanHDU <anhtuan02042002@gmail.com>
2023-11-23 09:47:41 +0700

    base nodejs

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const http = require("http");
+const app = express();
+
+const cors = require("cors");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors({ credentials: true, origin: "http://localhost:4200" }));
+const port = process.env.PORT || 8888;
+const hostname = process.env.HOST_NAME;
+
+const apiRoute = require("./src/routes/api");
+
+app.use("/api/v1", apiRoute);
+
+const server = http.createServer(app);
+
+(async () => {
+  try {
+    server.listen(port, hostname, () => {
+      console.log(`Server listening on port ${port}`);
+    });
+  } catch (error) {}
+})();
